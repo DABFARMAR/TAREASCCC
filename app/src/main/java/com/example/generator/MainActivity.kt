dcc97fdd3f719839aec40f204e886c900aeda803
F4B14NM4RT1N3Z <daborfabianmar@gmail.com>
2022-12-19 00:40:32 -0300

    F2

--- app/src/main/java/com/example/generator/MainActivity.kt
+++ app/src/main/java/com/example/generator/MainActivity.kt
@@ -5,6 +5,7 @@ import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import androidx.appcompat.app.AlertDialog
 import kotlin.random.Random
 
@@ -13,10 +14,18 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val recorrido: MutableList<String> = mutableListOf("ABCDEFGHIJKLMNOPQRSTUVWXYZ","abcdefghijklmnopqrstuvwxyz","0123456789","!#$%&/()?{}[]¿.,+-")
 
-        fun getRandPassword(n: Int): String
+        fun getRandPassword(n: Int,l: MutableList<String> ): String
         {
-            val characterSet = "!#$%&/()?{}[]¿0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+            val characterSet = StringBuilder()
+
+
+            for (i  in  l){
+                if (i != "false"){ characterSet.append((i)) }
+            }
+
 
             val random = Random(System.nanoTime())
             val password = StringBuilder()
@@ -36,30 +45,37 @@ class MainActivity : AppCompatActivity() {
         val ABC = findViewById<Button>(R.id.ABC)
         val abc = findViewById<Button>(R.id.abc)
         val numero = findViewById<Button>(R.id.num)
+        val cantidad = findViewById<EditText>(R.id.Cantidad)
 
         special_character.setOnClickListener{
             special_character.setTextColor(Color.GREEN)
+            recorrido[3] = false.toString()
         }
-
         ABC.setOnClickListener{
             ABC.setTextColor(Color.GREEN)
+            recorrido[0] = false.toString()
         }
 
         abc.setOnClickListener{
             abc.setTextColor(Color.GREEN)
+            recorrido[1] = false.toString()
         }
 
         numero.setOnClickListener{
             numero.setTextColor(Color.GREEN)
+            recorrido[2] = false.toString()
         }
 
         boton.setOnClickListener{
             AlertDialog.Builder(this).apply{
                 setTitle("Su contraseña es:" )
-                setMessage(getRandPassword(16))
+                setMessage(getRandPassword(cantidad.getText().toString().toInt(),recorrido))
                 setPositiveButton("Salir"){ _: DialogInterface, _:Int ->
                 }
-
+                ABC.setTextColor(Color.WHITE)
+                abc.setTextColor(Color.WHITE)
+                numero.setTextColor(Color.WHITE)
+                special_character.setTextColor(Color.WHITE)
 
             }.show()
 
